516956b3bb06571390bec9dfc299fab7b14b6acd
artur-calderon <artur.calderonart@gmail.com>
2026-05-05 19:14:45 -0300

    website

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
+
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || "production";
 const PORT = process.env.PORT || 3000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-	res.send("Hello from Express");
+	res.sendFile(path.join(__dirname, "src/views/home.html"));
+});
+
+app.get("/organizations", (req, res) => {
+	res.sendFile(path.join(__dirname, "src/views/organizations.html"));
+});
+
+app.get("/projects", (req, res) => {
+	res.sendFile(path.join(__dirname, "src/views/projects.html"));
 });
 
 app.listen(PORT, () => {
